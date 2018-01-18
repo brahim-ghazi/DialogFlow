c762f2fc53a85fd69c616e772cab5743d853299e
brahim-ghazi <brahim.ghazi@gmail.com>
2018-01-18 18:03:53 +0100

    allumer
    add allumer

--- index.js
+++ index.js
@@ -27,6 +27,28 @@ restService.post("/echo", function(req, res) {
   });
 });
 
+restService.post("/allumer", function(req, res) {
+  var intenisty =
+    req.body.result &&  req.body.result.parameters && req.body.result.parameters.intenisty ? req.body.result.parameters.intenisty  : "Seems like some problem. Speak again.";
+	
+  var temperature =
+	req.body.result &&  req.body.result.parameters && req.body.result.parameters.temperature ? req.body.result.parameters.temperature  : "Seems like some problem. Speak again.";
+	
+  if(intenisty == 150 && temperature == 2000){
+	  var msgErr = "BlueLignt protection is enable, you cannot apply this command."
+	   return res.json({
+		speech: msgErr,
+		displayText: msgErr,
+		source: "webhook-echo-sample"
+  }
+  
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
