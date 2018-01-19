03f866dc3c2435c26c8d25067606e1cbda68def6
brahim-ghazi <brahim.ghazi@gmail.com>
2018-01-19 10:45:10 +0100

    cfcf

--- index.js
+++ index.js
@@ -28,28 +28,74 @@ restService.post("/echo", function(req, res) {
 });
 
 restService.post("/allumer", function(req, res) {
-  var intenisty =
-    req.body.result &&  req.body.result.parameters && req.body.result.parameters.intenisty ? req.body.result.parameters.intenisty  : "Seems like some problem. Speak again.";
-	
-  var temperature =
-	req.body.result &&  req.body.result.parameters && req.body.result.parameters.temperature ? req.body.result.parameters.temperature  : "Seems like some problem. Speak again.";
-	
-  if(intenisty == 150 && temperature == 2200){
-	  var msgErr = "BlueLignt protection is enable, you cannot apply this command."
-	   return res.json({
-		speech: msgErr,
-		displayText: msgErr,
-		source: "EchoService"
-	 });
-  }
-  
-	  var msg = "The command with temperature "+ temperature +" and intenisty "+ intenisty +" was applied successfully" ;
-	   return res.json({
-		speech: msg,
-		displayText: msg,
-		source: "EchoService"
-	  });
-  
+
+    var isLogged = false;
+
+    var email = req.body.result.parameters.email;
+	if(email == null){
+		var emailRequired ="Email is required.Please enter your Email"
+		return res.json({
+                speech: emailRequired,
+                displayText: emailRequired,
+                source: "EchoService"
+            });
+	}
+	else{
+		
+		var password = req.body.result.parameters.email;
+		if(password == null){
+			var passwordRequired ="Password is required.Please enter your password"
+			return res.json({
+					speech: passwordRequired,
+					displayText: passwordRequired,
+					source: "EchoService"
+				});
+		}
+		else{
+			
+			var loginSuccess ="your username is "+email " and your password is : "+password;
+			return res.json({
+					speech: loginSuccess,
+					displayText: loginSuccess,
+					source: "EchoService"
+				});
+			
+		}
+	}
+    
+
+
+    if (isLogged) {
+        var intenisty =
+            req.body.result && req.body.result.parameters && req.body.result.parameters.intenisty ? req.body.result.parameters.intenisty : "Seems like some problem. Speak again.";
+
+        var temperature =
+            req.body.result && req.body.result.parameters && req.body.result.parameters.temperature ? req.body.result.parameters.temperature : "Seems like some problem. Speak again.";
+
+        if (intenisty == 150 && temperature == 2200) {
+            var msgErr = "BlueLignt protection is enable, you cannot apply this command."
+            return res.json({
+                speech: msgErr,
+                displayText: msgErr,
+                source: "EchoService"
+            });
+        }
+
+        var msg = "The command with temperature " + temperature + " and intenisty " + intenisty + " was applied successfully";
+        return res.json({
+            speech: msg,
+            displayText: msg,
+            source: "EchoService"
+        });
+    }
+//	esle{
+//		 var loginPlease = "Please login first, enter your username";
+//		 return res.json({
+//            speech: loginPlease,
+//            displayText: loginPlease,
+//            source: "EchoService"
+//        });
+//	}
 });
 
 
